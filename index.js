87372a3ed29f883f6b3c65f0cc3e9bbbdd6ce7b6
Victor Bejarano <alejandrobjrn4@gmail.com>
2023-11-15 19:02:59 -0500

    2. primer server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My App');
+const express = require('express');
+const app = express(); // Se crea aplicacion
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+});
