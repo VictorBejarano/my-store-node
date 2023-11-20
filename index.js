79d832a38980911c07129b536457b244042b4400
Victor Bejarano <alejandrobjrn4@gmail.com>
2023-11-20 17:49:22 -0500

    3. Peticion GET

--- index.js
+++ index.js
@@ -6,6 +6,42 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 3000,
+    },
+    {
+      name: 'Producto 2',
+      price: 4000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    name: 'Producto 1',
+    price: 3000,
+    id,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    name: 'Producto 1',
+    price: 3000,
+    categoryId,
+    productId,
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port' + port);
 });
