b7f1f740599e8052298a01959a3ea9d9670dcaa7
Victor Bejarano <alejandrobjrn4@gmail.com>
2023-11-20 20:22:51 -0500

    8. Capa de servicios

--- routes/product.router.js
+++ routes/product.router.js
@@ -1,35 +1,19 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductsServices = require('./../services/product.services');
 
 const router = express.Router();
+const service = new ProductsServices();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    });
-  }
+  const products = service.find();
   res.json(products);
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  if (id === '999') {
-    res.status(404).json({
-      message: 'not found',
-    });
-  } else {
-    res.json({
-      name: 'Producto 1',
-      price: 3000,
-      id,
-    });
-  }
+  const product = service.findOne(id);
+  res.json(product);
 });
 
 router.get('/filter', (req, res) => {
