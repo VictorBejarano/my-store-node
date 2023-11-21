14762fd961b5805b33a023e879675e9b09d2ae59
Victor Bejarano <alejandrobjrn4@gmail.com>
2023-11-21 18:09:21 -0500

    10. Async Await

--- services/product.services.js
+++ services/product.services.js
@@ -5,7 +5,7 @@ class ProductsServices {
     this.generate();
   }
 
-  generate() {
+  async generate() {
     const limit = 100;
     for (let index = 0; index < limit; index++) {
       this.products.push({
@@ -17,7 +17,7 @@ class ProductsServices {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.string.uuid(),
       ...data,
@@ -27,14 +27,18 @@ class ProductsServices {
   }
 
   find() {
-    return this.products;
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    });
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find((item) => item.id === id);
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex((item) => item.id === id);
 
     if (index === -1) {
@@ -45,7 +49,7 @@ class ProductsServices {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('product not found');
