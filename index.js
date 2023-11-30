9d8c90bb5e647baa35aa92984ad2a21fe62cc4fb
Victor Bejarano <alejandrobjrn4@gmail.com>
2023-11-30 17:49:55 -0500

    15. Solucion Cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const {
@@ -11,6 +12,19 @@ const app = express(); // Se crea aplicacion
 const port = 3000;
 
 app.use(express.json()); // Es necesario para recibir datos en JSON
+
+const whiteList = ['http://localhost:8080', 'http://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'), true);
+    }
+  },
+};
+
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
