95be246c447a1f0366b7532aa880daf6b9f6f685
Victor Bejarano <alejandrobjrn4@gmail.com>
2023-12-01 20:47:56 -0500

    19. Variables de entorno

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
 
+const config = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dvPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'victor',
-  password: 'admin123',
-  database: 'my_store',
+  connectionString: URI,
 });
 
 module.exports = pool;
